f22d3b0070f770a48114553614aec149c48826d2
Daniel Nikolaev <gusnikoda@student.gu.se>
2022-02-23 01:42:55 +0100

    Fixed the wrong type declaration

--- NewtonsMethod.hs
+++ NewtonsMethod.hs
@@ -1,13 +1,13 @@
-{-# LANGUAGE FlexibleContexts, FlexibleInstances #-}
-{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleContexts, FlexibleInstances,TypeOperators #-}
+{-# LANGUAGE TypeSynonymInstances#-}
 
 import Prelude hiding ((+), (-), (*), (/), negate, recip, (^),pi , sin, cos, exp, fromInteger, fromRational)
 import DSLsofMath.L.DSLsofMath.Algebra
 import DSLsofMath.L.DSLsofMath.FunExp 
 
 type Tri a = (a, a, a)
-type TriFun a = Tri (a ! a) -- = (a ! a, a ! a, a ! a)
-type FunTri a = a ! Tri a -- = a ! (a, a, a)
+type TriFun a = Tri (a -> a) -- = (a -> a, a ->  a, a -> a)
+type FunTri a = a -> Tri a -- = a -> (a, a, a)
 
 instance (Additive a ) => Additive (Tri a) where
     (+) = addTri 
@@ -24,29 +24,38 @@ instance (AddGroup a, MulGroup a) => MulGroup (Tri a) where
 
 addTri :: Additive a => Tri a -> Tri a -> Tri a
 addTri (a1,a2,a3) (a1',a2',a3') = (a1+a1',a2+a2',a3+a3') 
+
 zeroTri :: Additive a => Tri a
 zeroTri = (zero,zero,zero)
 
-oneTri :: Additive a => Tri a
+oneTri :: (Additive a, Multiplicative a) => Tri a
 oneTri = (one,one,one)
 
 mulTri :: Additive a => Tri a -> Tri a -> Tri a
 mulTri = undefined
 
-negateTri :: Additive a => Tri a -> Tri a -> Tri a
+negateTri :: Additive a => Tri a -> Tri a 
 negateTri = undefined
 
 recipTri :: Additive a => Tri a -> Tri a
 recipTri = undefined
 
-(addTri, zeroTri, mulTri, oneTri, negateTri, recipTri) = undefined
-
-
+--(addTri, zeroTri, mulTri, oneTri, negateTri, recipTri) = undefined
 
 instance Transcendental a => Transcendental (Tri a) where
-    pi = piTr
+    pi = piTri
     sin = sinTri
     cos = cosTri
-    exp = expTr
+    exp = expTri
 
-(piTri, sinTri, cosTri, expTri) = undefined
+piTri :: Transcendental a => Tri a
+piTri = undefined 
+
+sinTri :: Transcendental a => Tri a -> Tri a
+sinTri = undefined 
+
+cosTri ::Transcendental a => Tri a -> Tri a
+cosTri = undefined 
+
+expTri ::Transcendental a => Tri a -> Tri a
+expTri = undefined 
